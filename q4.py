6846ae1dd03d3e3ff47b95a9e7a85d9754636a7f
Temperature <chengputan@gmail.com>
2021-01-29 20:53:43 -0600

    question4 finished and tested

---
+++ q4.py
@@ -0,0 +1,68 @@
+def Add(numbers):
+    if len(numbers) == 0:
+        return 0
+    elif numbers.startswith('//'):
+        newline_index = numbers.find('\n')
+        delimiter = numbers[2:newline_index]
+        numbers = numbers[newline_index+1:len(numbers)]
+        
+        integers = [int(number.rstrip()) for number in numbers.split(delimiter)]
+
+        exception_occurred = False
+        negatives = []
+        sum = 0
+        for integer in integers:
+            if integer < 0:
+                exception_occurred = True
+                negatives.append(integer)
+            else:
+                sum = sum + integer
+        if exception_occurred == False:
+            return sum
+        else:
+            raise Exception('Negatives not allowed', negatives)
+
+    else:
+        integers = [int(number.rstrip()) for number in numbers.split(',')]
+
+        exception_occurred = False
+        negatives = []
+        sum = 0
+        for integer in integers:
+            if integer < 0:
+                exception_occurred = True
+                negatives.append(integer)
+            else:
+                sum = sum + integer
+        if exception_occurred == False:
+            return sum
+        else:
+            raise Exception('Negatives not allowed', negatives)
+
+print("Testing start.")
+
+print(Add("//$\n1$-2$-3"))
+# comment the above line and comment the following line out to test it
+# print(Add("-1,2,-8")) 
+
+if Add("//$\n1$2$3") != 6:
+    print("Error: expected Add('//$\n1$2$3') to be 6, got: ", Add("//$\n1$2$3"), )
+
+if Add("//@\n2@3@8") != 13:
+    print("Error: expected Add('//@\n2@3@8') to be 13, got: ", Add("//@\n2@3@8"), )
+
+if Add("1\n,\n2,5") != 8:
+    print("Error: expected Add('1\n,\n2,5') to be 8, got: ", Add("1\n,\n2,5"), )
+
+if Add("0,8,\n9") != 17:
+    print("Error: expected Add('0,8,\n9') to be 17, got: ", Add("0,8,\n9"), )
+
+if Add("1,2,5") != 8:
+    print("Error: expected Add('1,2,5') to be 8, got: ", Add("1,2,5"), )
+
+if Add("0,8,9") != 17:
+    print("Error: expected Add('0,8,9') to be 17, got: ", Add("0,8,9"), )
+
+print("Testing finished.")
+
+
